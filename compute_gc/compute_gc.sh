6b3ab8d1f5782368ddb20c17c254db95a0a3986f
anna shcherbina <annashcherbina@gmail.com>
2020-06-07 16:20:00 -0700

    script to compute gc content in a binned model

---
+++ compute_gc/compute_gc.sh
@@ -0,0 +1,18 @@
+echo "GATA1"
+python compute_gc.py --bed /mnt/lab_data2/annashch/locusselect_examples/coordinates/GATA1.geneLocus.250bp.windows.hg19.bed --ref_fasta /users/annashch/male.hg19.fa --outf gc.GATA1.bed
+
+#python compute_gc.py --bed /mnt/lab_data2/annashch/locusselect_examples/coordinates/MYC.geneLocus.250bp.windows.hg19.bed --ref_fasta /users/annashch/male.hg19.fa --outf gc.MYC.bed
+#echo "MYC"
+#python compute_gc.py --bed /mnt/lab_data2/annashch/locusselect_examples/coordinates/SOX2.geneLocus.250bp.windows.hg19.bed --ref_fasta /users/annashch/male.hg19.fa --outf gc.SOX2.bed
+#echo "SOX2" 
+#python compute_gc.py --bed /mnt/lab_data2/annashch/locusselect_examples/coordinates/NANOG.geneLocus.250bp.windows.hg19.bed --ref_fasta /users/annashch/male.hg19.fa --outf gc.NANOG.bed
+#echo "NANOG"
+#python compute_gc.py --bed /mnt/lab_data2/annashch/locusselect_examples/coordinates/POU5F1.geneLocus.250bp.windows.hg19.bed --ref_fasta /users/annashch/male.hg19.fa --outf gc.POU5F1.bed 
+#echo "POU5F1" 
+
+#python compute_gc.py --bed /mnt/lab_data2/annashch/locusselect_examples/coordinates/coordinates_0_BCL11A.bed --ref_fasta /users/annashch/male.hg19.fa --outf gc.BCL11A.bed
+#python compute_gc.py --bed /mnt/lab_data2/annashch/locusselect_examples/coordinates/coordinates_4_MYC.bed --ref_fasta /users/annashch/male.hg19.fa --outf gc.MYC.bed
+#python compute_gc.py --bed /mnt/lab_data2/annashch/locusselect_examples/coordinates/coordinates_2_HBE1.bed --ref_fasta /users/annashch/male.hg19.fa --outf gc.HBE1.bed
+#python compute_gc.py --bed /mnt/lab_data2/annashch/locusselect_examples/coordinates/coordinates_1_HBA2.bed --ref_fasta /users/annashch/male.hg19.fa --outf gc.HBA2.bed
+#python compute_gc.py --bed /mnt/lab_data2/annashch/locusselect_examples/coordinates/coordinates_5_RBM38.bed --ref_fasta /users/annashch/male.hg19.fa --outf gc.RBM38.bed
+#python compute_gc.py --bed /mnt/lab_data2/annashch/locusselect_examples/coordinates/coordinates_3_LM02.bed --ref_fasta /users/annashch/male.hg19.fa --outf gc.LM02.bed
